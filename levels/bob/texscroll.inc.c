a6f5f448307d2d62b4772c2818122ae1d7158a2f
MrComit <yozemight@gmail.com>
2020-12-04 22:44:03 -0500

    counter

--- levels/bob/texscroll.inc.c
+++ levels/bob/texscroll.inc.c
@@ -86,6 +86,46 @@ void scroll_bob_dl_Kitchen_001_mesh_vtx_0() {
 	currentY += deltaY;
 
 }
+void scroll_bob_dl_KitchenGround_005_mesh_vtx_0() {
+	int i = 0;
+	int count = 4;
+	int width = 32 * 0x20;
+	int height = 32 * 0x20;
+
+	static int currentX = 0;
+	int deltaX;
+	static int timeX;
+	float amplitudeX = 0.20000000298023224;
+	float frequencyX = 0.20000000298023224;
+	float offsetX = 0.0;
+	static int currentY = 0;
+	int deltaY;
+	static int timeY;
+	float amplitudeY = 0.20000000298023224;
+	float frequencyY = 0.20000000298023224;
+	float offsetY = 0.0;
+	Vtx *vertices = segmented_to_virtual(bob_dl_KitchenGround_005_mesh_vtx_0);
+
+	deltaX = (int)(amplitudeX * frequencyX * coss((frequencyX * timeX + offsetX) * (1024 * 16 - 1) / 6.28318530718) * 0x20);
+	deltaY = (int)(amplitudeY * frequencyY * coss((frequencyY * timeY + offsetY) * (1024 * 16 - 1) / 6.28318530718) * 0x20);
+
+	if (absi(currentX) > width) {
+		deltaX -= (int)(absi(currentX) / width) * width * signum_positive(deltaX);
+	}
+	if (absi(currentY) > height) {
+		deltaY -= (int)(absi(currentY) / height) * height * signum_positive(deltaY);
+	}
+
+	for (i = 0; i < count; i++) {
+		vertices[i].n.tc[0] += deltaX;
+		vertices[i].n.tc[1] += deltaY;
+	}
+	currentX += deltaX;
+	timeX += 1;
+	currentY += deltaY;
+	timeY += 1;
+
+}
 void scroll_bob_dl_Steam_mesh_vtx_0() {
 	int i = 0;
 	int count = 8;
@@ -113,6 +153,7 @@ void scroll_bob() {
 	scroll_bob_dl_MainHall_003_mesh_vtx_0();
 	scroll_bob_dl_Kitchen_mesh_vtx_2();
 	scroll_bob_dl_Kitchen_001_mesh_vtx_0();
+	scroll_bob_dl_KitchenGround_005_mesh_vtx_0();
 	scroll_bob_dl_Steam_mesh_vtx_0();
 
 }
