1dfd96412cca734c47feb031ebd5e11f54a15706
MrComit <yozemight@gmail.com>
2020-12-12 17:58:46 -0500

    living room design

--- levels/bob/texscroll.inc.c
+++ levels/bob/texscroll.inc.c
@@ -42,6 +42,50 @@ void scroll_bob_dl_MainHall_003_mesh_vtx_0() {
 	currentY += deltaY;
 
 }
+void scroll_bob_dl_Hallway_mesh_vtx_2() {
+	int i = 0;
+	int count = 4;
+	int width = 64 * 0x20;
+	int height = 64 * 0x20;
+
+	static int currentY = 0;
+	int deltaY;
+	Vtx *vertices = segmented_to_virtual(bob_dl_Hallway_mesh_vtx_2);
+
+	deltaY = (int)(0.3499999940395355 * 0x20) % height;
+
+	if (absi(currentY) > height) {
+		deltaY -= (int)(absi(currentY) / height) * height * signum_positive(deltaY);
+	}
+
+	for (i = 0; i < count; i++) {
+		vertices[i].n.tc[1] += deltaY;
+	}
+	currentY += deltaY;
+
+}
+void scroll_bob_dl_Hallway_002_mesh_vtx_0() {
+	int i = 0;
+	int count = 4;
+	int width = 64 * 0x20;
+	int height = 64 * 0x20;
+
+	static int currentY = 0;
+	int deltaY;
+	Vtx *vertices = segmented_to_virtual(bob_dl_Hallway_002_mesh_vtx_0);
+
+	deltaY = (int)(0.3499999940395355 * 0x20) % height;
+
+	if (absi(currentY) > height) {
+		deltaY -= (int)(absi(currentY) / height) * height * signum_positive(deltaY);
+	}
+
+	for (i = 0; i < count; i++) {
+		vertices[i].n.tc[1] += deltaY;
+	}
+	currentY += deltaY;
+
+}
 void scroll_bob_dl_Kitchen_mesh_vtx_2() {
 	int i = 0;
 	int count = 4;
@@ -148,12 +192,60 @@ void scroll_bob_dl_Steam_mesh_vtx_0() {
 	currentY += deltaY;
 
 }
+void scroll_bob_dl_LivingRoom_mesh_vtx_2() {
+	int i = 0;
+	int count = 7;
+	int width = 64 * 0x20;
+	int height = 64 * 0x20;
+
+	static int currentY = 0;
+	int deltaY;
+	Vtx *vertices = segmented_to_virtual(bob_dl_LivingRoom_mesh_vtx_2);
+
+	deltaY = (int)(0.3499999940395355 * 0x20) % height;
+
+	if (absi(currentY) > height) {
+		deltaY -= (int)(absi(currentY) / height) * height * signum_positive(deltaY);
+	}
+
+	for (i = 0; i < count; i++) {
+		vertices[i].n.tc[1] += deltaY;
+	}
+	currentY += deltaY;
+
+}
+void scroll_bob_dl_LivingRoom_001_mesh_vtx_0() {
+	int i = 0;
+	int count = 5;
+	int width = 64 * 0x20;
+	int height = 64 * 0x20;
+
+	static int currentY = 0;
+	int deltaY;
+	Vtx *vertices = segmented_to_virtual(bob_dl_LivingRoom_001_mesh_vtx_0);
+
+	deltaY = (int)(0.3499999940395355 * 0x20) % height;
+
+	if (absi(currentY) > height) {
+		deltaY -= (int)(absi(currentY) / height) * height * signum_positive(deltaY);
+	}
+
+	for (i = 0; i < count; i++) {
+		vertices[i].n.tc[1] += deltaY;
+	}
+	currentY += deltaY;
+
+}
 void scroll_bob() {
 	scroll_bob_dl_MainHall_002_mesh_vtx_0();
 	scroll_bob_dl_MainHall_003_mesh_vtx_0();
+	scroll_bob_dl_Hallway_mesh_vtx_2();
+	scroll_bob_dl_Hallway_002_mesh_vtx_0();
 	scroll_bob_dl_Kitchen_mesh_vtx_2();
 	scroll_bob_dl_Kitchen_001_mesh_vtx_0();
 	scroll_bob_dl_KitchenGround_005_mesh_vtx_0();
 	scroll_bob_dl_Steam_mesh_vtx_0();
+	scroll_bob_dl_LivingRoom_mesh_vtx_2();
+	scroll_bob_dl_LivingRoom_001_mesh_vtx_0();
 
 }
