5f3080b0c452207f4d2be561ce3dd96ec704edff
MrComit <yozemight@gmail.com>
2021-01-21 00:10:17 -0500

    xd

--- src/game/save_file.h
+++ src/game/save_file.h
@@ -50,12 +50,7 @@ struct MainMenuSaveData
     u32 coinScoreAges[NUM_SAVE_FILES];
     u16 soundMode;
 
-#ifdef VERSION_EU
-    u16 language;
-#define SUBTRAHEND 8
-#else
 #define SUBTRAHEND 6
-#endif
 
     // Pad to match the EEPROM size of 0x200 (10 bytes on JP/US, 8 bytes on EU)
     u8 filler[EEPROM_SIZE / 2 - SUBTRAHEND - NUM_SAVE_FILES * (4 + sizeof(struct SaveFile))];
