790ca11e2299d4640b63e04dd6039f5a73418110
MrComit <yozemight@gmail.com>
2021-02-14 23:20:36 -0500

    tree n lightning

--- levels/wf/texscroll.inc.c
+++ levels/wf/texscroll.inc.c
@@ -72,8 +72,39 @@ void scroll_wf_dl_Cloud_002_mesh_layer_4_vtx_0() {
 	currentY += deltaY;
 
 }
+void scroll_wf_dl_Lightning_mesh_layer_4_vtx_0() {
+	int i = 0;
+	int count = 24;
+	int width = 16 * 0x20;
+	int height = 32 * 0x20;
+
+	static int currentX = 0;
+	int deltaX;
+	static int currentY = 0;
+	int deltaY;
+	Vtx *vertices = segmented_to_virtual(wf_dl_Lightning_mesh_layer_4_vtx_0);
+
+	deltaX = (int)(4.849999904632568 * 0x20 * random_float() * random_sign()) % width;
+	deltaY = (int)(3.0799999237060547 * 0x20 * random_float() * random_sign()) % height;
+
+	if (absi(currentX) > width) {
+		deltaX -= (int)(absi(currentX) / width) * width * signum_positive(deltaX);
+	}
+	if (absi(currentY) > height) {
+		deltaY -= (int)(absi(currentY) / height) * height * signum_positive(deltaY);
+	}
+
+	for (i = 0; i < count; i++) {
+		vertices[i].n.tc[0] += deltaX;
+		vertices[i].n.tc[1] += deltaY;
+	}
+	currentX += deltaX;
+	currentY += deltaY;
+
+}
 void scroll_wf() {
 	scroll_wf_dl_Hallway_mesh_layer_1_vtx_4();
 	scroll_wf_dl_Hallway_005_mesh_layer_6_vtx_0();
 	scroll_wf_dl_Cloud_002_mesh_layer_4_vtx_0();
+	scroll_wf_dl_Lightning_mesh_layer_4_vtx_0();
 }
