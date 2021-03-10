4a01fef9d28e7332955863c0a673f24cc5a9fed3
MrComit <yozemight@gmail.com>
2021-03-10 18:35:34 -0500

    light tweak

--- src/game/behaviors/mirror_room.inc.c
+++ src/game/behaviors/mirror_room.inc.c
@@ -7,24 +7,25 @@ void bhv_mirror_light_init(void) {
 void bhv_mirror_light_loop(void) {
     Vec3f pos;
     struct Surface *wall;
+    struct Object *obj;
     //o->oFaceAngleYaw += 0x400;
     switch (o->oAction) {
         case 0:
-            o->header.gfx.scale[1] += 0.3f;
+            o->header.gfx.scale[1] += 1.0f;
             pos[1] = o->oPosY;
             pos[0] = o->oPosX + ((o->header.gfx.scale[1] * 100.0f) * sins(o->oFaceAngleYaw));
             pos[2] = o->oPosZ + ((o->header.gfx.scale[1] * 100.0f) * coss(o->oFaceAngleYaw));
             o->oSurfF4 = resolve_and_return_wall_collisions(pos, 0, 30.0f);
-            if (o->oSurfF4 == NULL || o->oSurfF4->type != SURFACE_MIRROR) {
-                break;
+            if (o->oSurfF4 != NULL) {
+                o->oAction = 1;
+                if (o->oSurfF4->type == SURFACE_MIRROR) {
+                    obj = spawn_object(o, MODEL_MIRROR_LIGHT, bhvMirrorLight);
+                    obj->oFaceAngleYaw = atan2s(o->oSurfF4->normal.z, o->oSurfF4->normal.x);
+                    vec3f_copy(&obj->oPosX, pos);
+                }
             }
-            o->oFaceAngleYaw = atan2s(o->oSurfF4->normal.z, o->oSurfF4->normal.x);
-            o->header.gfx.scale[1] = 0.1f;
-            vec3f_copy(&o->oPosX, pos);
-            o->oAction = 1;
             break;
         case 1:
-            o->oAction = 0;
             break;
     }
 }
