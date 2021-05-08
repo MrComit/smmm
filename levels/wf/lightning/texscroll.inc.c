503c4282c083e3c95b7014e764274a5383e16c4e
MrComit <yozemight@gmail.com>
2021-05-08 18:43:42 -0400

    balcony polishing

--- levels/wf/lightning/texscroll.inc.c
+++ levels/wf/lightning/texscroll.inc.c
@@ -1,3 +1,25 @@
+void scroll_lightning_lightning_mesh_layer_1_vtx_0() {
+	int i = 0;
+	int count = 61;
+	int width = 16 * 0x20;
+	int height = 16 * 0x20;
+
+	static int currentY = 0;
+	int deltaY;
+	Vtx *vertices = segmented_to_virtual(lightning_lightning_mesh_layer_1_vtx_0);
+
+	deltaY = (int)(2.109999895095825 * 0x20 * random_float() * random_sign()) % height;
+
+	if (absi(currentY) > height) {
+		deltaY -= (int)(absi(currentY) / height) * height * signum_positive(deltaY);
+	}
+
+	for (i = 0; i < count; i++) {
+		vertices[i].n.tc[1] += deltaY;
+	}
+	currentY += deltaY;
+
+}
 void scroll_lightning_lightning_mesh_layer_4_vtx_0() {
 	int i = 0;
 	int count = 24;
@@ -29,5 +51,6 @@ void scroll_lightning_lightning_mesh_layer_4_vtx_0() {
 
 }
 void scroll_wf_level_geo_lightning() {
+	scroll_lightning_lightning_mesh_layer_1_vtx_0();
 	scroll_lightning_lightning_mesh_layer_4_vtx_0();
 }
--- levels/wf/lightning/texscroll.inc.h
+++ levels/wf/lightning/texscroll.inc.h
@@ -1,2 +1,3 @@
+extern void scroll_lightning_lightning_mesh_layer_1_vtx_0();
 extern void scroll_lightning_lightning_mesh_layer_4_vtx_0();
 extern void scroll_wf_level_geo_lightning();
