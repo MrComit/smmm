3b460099fd22c71276691873f0ba48454dcae605
MrComit <yozemight@gmail.com>
2021-05-13 15:39:47 -0400

    lounge touch up

--- levels/wf/texscroll.inc.c
+++ levels/wf/texscroll.inc.c
@@ -1,3 +1,25 @@
+void scroll_wf_dl_Hallway_005_mesh_layer_1_vtx_5() {
+	int i = 0;
+	int count = 9;
+	int width = 64 * 0x20;
+	int height = 64 * 0x20;
+
+	static int currentY = 0;
+	int deltaY;
+	Vtx *vertices = segmented_to_virtual(wf_dl_Hallway_005_mesh_layer_1_vtx_5);
+
+	deltaY = (int)(0.3499999940395355 * 0x20) % height;
+
+	if (absi(currentY) > height) {
+		deltaY -= (int)(absi(currentY) / height) * height * signum_positive(deltaY);
+	}
+
+	for (i = 0; i < count; i++) {
+		vertices[i].n.tc[1] += deltaY;
+	}
+	currentY += deltaY;
+
+}
 void scroll_wf_dl_Hallway_005_mesh_layer_6_vtx_0() {
 	int i = 0;
 	int count = 72;
@@ -18,28 +40,6 @@ void scroll_wf_dl_Hallway_005_mesh_layer_6_vtx_0() {
 		vertices[i].n.tc[0] += deltaX;
 	}
 	currentX += deltaX;
-
-}
-void scroll_wf_dl_Hallway_005_mesh_layer_1_vtx_4() {
-	int i = 0;
-	int count = 5;
-	int width = 64 * 0x20;
-	int height = 64 * 0x20;
-
-	static int currentY = 0;
-	int deltaY;
-	Vtx *vertices = segmented_to_virtual(wf_dl_Hallway_005_mesh_layer_1_vtx_4);
-
-	deltaY = (int)(0.3499999940395355 * 0x20) % height;
-
-	if (absi(currentY) > height) {
-		deltaY -= (int)(absi(currentY) / height) * height * signum_positive(deltaY);
-	}
-
-	for (i = 0; i < count; i++) {
-		vertices[i].n.tc[1] += deltaY;
-	}
-	currentY += deltaY;
 
 }
 void scroll_wf_dl_Steam_mesh_layer_5_vtx_0() {
@@ -131,8 +131,8 @@ void scroll_wf_dl_Office_mesh_layer_1_vtx_3() {
 
 }
 void scroll_wf() {
+	scroll_wf_dl_Hallway_005_mesh_layer_1_vtx_5();
 	scroll_wf_dl_Hallway_005_mesh_layer_6_vtx_0();
-	scroll_wf_dl_Hallway_005_mesh_layer_1_vtx_4();
 	scroll_wf_dl_Steam_mesh_layer_5_vtx_0();
 	scroll_wf_dl_Den_mesh_layer_1_vtx_3();
 	scroll_wf_dl_Servant_s_Quarters_mesh_layer_1_vtx_3();
