79ec9d9532c9705db6568b3908cf465fc7c753e1
MrComit <yozemight@gmail.com>
2021-07-05 15:24:33 -0400

    waterfall stuff

--- levels/bbh/texscroll.inc.h
+++ levels/bbh/texscroll.inc.h
@@ -1,5 +1,7 @@
 extern void scroll_bbh_dl_Grass_mesh_layer_5_vtx_0();
 extern void scroll_bbh_dl_Water_mesh_layer_5_vtx_0();
-extern void scroll_bbh_dl_Waterfall_mesh_layer_2_vtx_0();
 extern void scroll_bbh_dl_Waterfall_mesh_layer_5_vtx_0();
+extern void scroll_bbh_dl_Waterfall_mesh_layer_5_vtx_1();
+extern void scroll_bbh_dl_Waterfall_mesh_layer_5_vtx_2();
+extern void scroll_bbh_dl_Waterfall_mesh_layer_2_vtx_0();
 extern void scroll_bbh();
