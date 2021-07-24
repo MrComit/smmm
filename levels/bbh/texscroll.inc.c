f0925908d2cd6fb0230be0c598654daf272f660d
MrComit <yozemight@gmail.com>
2021-07-24 18:40:58 -0400

    castle modelling with blue coin switch

--- levels/bbh/texscroll.inc.c
+++ levels/bbh/texscroll.inc.c
@@ -186,7 +186,7 @@ void scroll_bbh_dl_Waterfall_001_mesh_layer_5_vtx_1() {
 }
 void scroll_bbh_dl_BB_mesh_layer_5_vtx_0() {
 	int i = 0;
-	int count = 15;
+	int count = 30;
 	int width = 32 * 0x20;
 	int height = 32 * 0x20;
 
@@ -194,13 +194,13 @@ void scroll_bbh_dl_BB_mesh_layer_5_vtx_0() {
 	int deltaX;
 	static int timeX;
 	float amplitudeX = 1.0;
-	float frequencyX = 0.10000000149011612;
+	float frequencyX = 0.05000000074505806;
 	float offsetX = 32.0;
 	static int currentY = 0;
 	int deltaY;
 	static int timeY;
 	float amplitudeY = 1.0;
-	float frequencyY = 0.10000000149011612;
+	float frequencyY = 0.05000000074505806;
 	float offsetY = 0.0;
 	Vtx *vertices = segmented_to_virtual(bbh_dl_BB_mesh_layer_5_vtx_0);
 
@@ -224,7 +224,7 @@ void scroll_bbh_dl_BB_mesh_layer_5_vtx_0() {
 	timeY += 1;
 
 }
-void scroll_bbh_dl_Snow_mesh_layer_1_vtx_3() {
+void scroll_bbh_dl_Snow_mesh_layer_1_vtx_2() {
 	int i = 0;
 	int count = 14;
 	int width = 32 * 0x20;
@@ -242,7 +242,7 @@ void scroll_bbh_dl_Snow_mesh_layer_1_vtx_3() {
 	float amplitudeY = 1.0;
 	float frequencyY = 0.10000000149011612;
 	float offsetY = 0.0;
-	Vtx *vertices = segmented_to_virtual(bbh_dl_Snow_mesh_layer_1_vtx_3);
+	Vtx *vertices = segmented_to_virtual(bbh_dl_Snow_mesh_layer_1_vtx_2);
 
 	deltaX = (int)(amplitudeX * frequencyX * coss((frequencyX * timeX + offsetX) * (1024 * 16 - 1) / 6.28318530718) * 0x20);
 	deltaY = (int)(amplitudeY * frequencyY * coss((frequencyY * timeY + offsetY) * (1024 * 16 - 1) / 6.28318530718) * 0x20);
@@ -272,5 +272,5 @@ void scroll_bbh() {
 	scroll_bbh_dl_Waterfall_001_mesh_layer_5_vtx_0();
 	scroll_bbh_dl_Waterfall_001_mesh_layer_5_vtx_1();
 	scroll_bbh_dl_BB_mesh_layer_5_vtx_0();
-	scroll_bbh_dl_Snow_mesh_layer_1_vtx_3();
+	scroll_bbh_dl_Snow_mesh_layer_1_vtx_2();
 }
