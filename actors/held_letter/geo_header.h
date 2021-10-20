f9310242fd93d482bc5d963777513200c33e8b61
MrComit <yozemight@gmail.com>
2021-10-19 23:56:52 -0400

    made letter paper thin

--- actors/held_letter/geo_header.h
+++ actors/held_letter/geo_header.h
@@ -1,6 +1,6 @@
 extern const GeoLayout held_letter_geo[];
 extern u8 held_letter_letter_rgba16[];
-extern Vtx held_letter_Letter_mesh_layer_1_vtx_0[24];
+extern Vtx held_letter_Letter_mesh_layer_1_vtx_0[8];
 extern Gfx held_letter_Letter_mesh_layer_1_tri_0[];
 extern Gfx mat_held_letter_Letter_001[];
 extern Gfx held_letter_Letter_mesh_layer_1[];
