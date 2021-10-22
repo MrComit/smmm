c127e6771e6a882e8cc6fcceb5ce2e86d04321f7
MrComit <yozemight@gmail.com>
2021-10-22 01:02:54 -0400

    replaced mario model

--- src/game/texscroll/group0_texscroll.inc.c
+++ src/game/texscroll/group0_texscroll.inc.c
@@ -1,12 +1,10 @@
 #include "actors/group0.h"
 #include "actors/ghostsand_ball/texscroll.inc.c"
 #include "actors/teleporter/texscroll.inc.c"
-#include "actors/mario/texscroll.inc.c"
 //#include "actors/lantern/texscroll.inc.c"
 void scroll_textures_group0() {
 	scroll_actor_geo_ghostsand_ball();
 	scroll_actor_geo_teleporter();
-	scroll_actor_geo_mario();
-
+	
 	//scroll_actor_geo_lantern();
 }
--- src/game/texscroll/group0_texscroll.inc.h
+++ src/game/texscroll/group0_texscroll.inc.h
@@ -1,5 +1,4 @@
 #include "actors/teleporter/texscroll.inc.h"
 #include "actors/ghostsand_ball/texscroll.inc.h"
 #include "actors/lantern/texscroll.inc.h"
-#include "actors/mario/texscroll.inc.h"
 extern void scroll_textures_group0();
