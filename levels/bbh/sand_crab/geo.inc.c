80d9d77989db37037c37cc3cfa86e3666bafeccd
MrComit <yozemight@gmail.com>
2021-11-04 00:37:47 -0400

    unanimated crab model

--- levels/bbh/sand_crab/geo.inc.c
+++ levels/bbh/sand_crab/geo.inc.c
@@ -3,7 +3,7 @@
 const GeoLayout sand_crab_geo[] = {
 	GEO_CULLING_RADIUS(4096),
 	GEO_OPEN_NODE(),
-		GEO_DISPLAY_LIST(LAYER_OPAQUE, sand_crab_sand_crab_mesh_layer_1),
+		GEO_DISPLAY_LIST(LAYER_OPAQUE, sand_crab_crab_mesh_layer_1),
 		GEO_DISPLAY_LIST(LAYER_OPAQUE, sand_crab_material_revert_render_settings),
 	GEO_CLOSE_NODE(),
 	GEO_END(),
