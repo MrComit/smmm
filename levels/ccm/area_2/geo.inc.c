87bef948e55eeaa41650fd923975adaf3bd58bc8
MrComit <yozemight@gmail.com>
2022-01-25 22:24:57 -0500

    a lil decorating

--- levels/ccm/area_2/geo.inc.c
+++ levels/ccm/area_2/geo.inc.c
@@ -31,6 +31,7 @@ const GeoLayout ccm_dl_202__NULL_geo[] = {
 const GeoLayout ccm_dl_203__City2_geo[] = {
 	GEO_NODE_START(),
 	GEO_OPEN_NODE(),
+		GEO_DISPLAY_LIST(LAYER_OPAQUE, ccm_dl_Board_mesh_layer_1),
 		GEO_DISPLAY_LIST(LAYER_OPAQUE, ccm_dl_Bridge_002_mesh_layer_1),
 		GEO_DISPLAY_LIST(LAYER_OPAQUE, ccm_dl_House_012_mesh_layer_1),
 		GEO_DISPLAY_LIST(LAYER_OPAQUE, ccm_dl_Lego_004_mesh_layer_1),
