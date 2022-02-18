57d79e5142e4baaf762ac6aa11eb8d2bd8882b8d
MrComit <yozemight@gmail.com>
2022-02-17 22:12:49 -0500

    game room koopas

--- actors/new_koopa/geo.inc.c
+++ actors/new_koopa/geo.inc.c
@@ -1,20 +1,20 @@
 #include "src/game/envfx_snow.h"
 
-const GeoLayout new_koopa_root_SpoonArma_001[] = {
+const GeoLayout new_koopa_root_ASpoonArma_001[] = {
 	GEO_NODE_START(),
 	GEO_OPEN_NODE(),
 		GEO_DISPLAY_LIST(LAYER_OPAQUE, new_koopa_root_root_spoon_mesh_layer_1),
 	GEO_CLOSE_NODE(),
 	GEO_RETURN(),
 };
-const GeoLayout new_koopa_root_KnifeArma_002[] = {
+const GeoLayout new_koopa_root_AKnifeArma_002[] = {
 	GEO_NODE_START(),
 	GEO_OPEN_NODE(),
 		GEO_DISPLAY_LIST(LAYER_OPAQUE, new_koopa_root_root_knife_mesh_layer_1),
 	GEO_CLOSE_NODE(),
 	GEO_RETURN(),
 };
-const GeoLayout new_koopa_ForkArma[] = {
+const GeoLayout new_koopa_AForkArma[] = {
 	GEO_NODE_START(),
 	GEO_OPEN_NODE(),
 		GEO_SWITCH_CASE(3, geo_switch_bparam2),
@@ -23,27 +23,27 @@ const GeoLayout new_koopa_ForkArma[] = {
 			GEO_OPEN_NODE(),
 				GEO_DISPLAY_LIST(LAYER_OPAQUE, new_koopa_root_fork_mesh_layer_1),
 			GEO_CLOSE_NODE(),
-			GEO_BRANCH(1, new_koopa_root_SpoonArma_001),
-			GEO_BRANCH(1, new_koopa_root_KnifeArma_002),
+			GEO_BRANCH(1, new_koopa_root_ASpoonArma_001),
+			GEO_BRANCH(1, new_koopa_root_AKnifeArma_002),
 		GEO_CLOSE_NODE(),
 	GEO_CLOSE_NODE(),
 	GEO_RETURN(),
 };
-const GeoLayout new_koopa_root_StaplerArma[] = {
+const GeoLayout new_koopa_root_BStaplerArma[] = {
 	GEO_NODE_START(),
 	GEO_OPEN_NODE(),
 		GEO_DISPLAY_LIST(LAYER_OPAQUE, new_koopa_root_root_dl_mesh_layer_1),
 	GEO_CLOSE_NODE(),
 	GEO_RETURN(),
 };
-const GeoLayout new_koopa_root_PenArma[] = {
+const GeoLayout new_koopa_root_BPenArma[] = {
 	GEO_NODE_START(),
 	GEO_OPEN_NODE(),
 		GEO_DISPLAY_LIST(LAYER_OPAQUE, new_koopa_root_root_pendl_mesh_layer_1),
 	GEO_CLOSE_NODE(),
 	GEO_RETURN(),
 };
-const GeoLayout new_koopa_PhoneArma[] = {
+const GeoLayout new_koopa_BPhoneArma[] = {
 	GEO_NODE_START(),
 	GEO_OPEN_NODE(),
 		GEO_SWITCH_CASE(3, geo_switch_bparam2),
@@ -52,16 +52,45 @@ const GeoLayout new_koopa_PhoneArma[] = {
 			GEO_OPEN_NODE(),
 				GEO_DISPLAY_LIST(LAYER_OPAQUE, new_koopa_root_phonedl_mesh_layer_1),
 			GEO_CLOSE_NODE(),
-			GEO_BRANCH(1, new_koopa_root_StaplerArma),
-			GEO_BRANCH(1, new_koopa_root_PenArma),
+			GEO_BRANCH(1, new_koopa_root_BStaplerArma),
+			GEO_BRANCH(1, new_koopa_root_BPenArma),
 		GEO_CLOSE_NODE(),
 	GEO_CLOSE_NODE(),
 	GEO_RETURN(),
 };
-const GeoLayout new_koopa_IcicleArma[] = {
+const GeoLayout new_koopa_CIcicleArma[] = {
 	GEO_NODE_START(),
 	GEO_OPEN_NODE(),
 		GEO_DISPLAY_LIST(LAYER_OPAQUE, new_koopa_root_dl_mesh_layer_1),
+	GEO_CLOSE_NODE(),
+	GEO_RETURN(),
+};
+const GeoLayout new_koopa_root_DHammer[] = {
+	GEO_NODE_START(),
+	GEO_OPEN_NODE(),
+		GEO_DISPLAY_LIST(LAYER_OPAQUE, new_koopa_root_root_hammer_mesh_layer_1),
+	GEO_CLOSE_NODE(),
+	GEO_RETURN(),
+};
+const GeoLayout new_koopa_root_DPaddle[] = {
+	GEO_NODE_START(),
+	GEO_OPEN_NODE(),
+		GEO_DISPLAY_LIST(LAYER_OPAQUE, new_koopa_root_root_paddle_mesh_layer_1),
+	GEO_CLOSE_NODE(),
+	GEO_RETURN(),
+};
+const GeoLayout new_koopa_DCue[] = {
+	GEO_NODE_START(),
+	GEO_OPEN_NODE(),
+		GEO_SWITCH_CASE(3, geo_switch_bparam2),
+		GEO_OPEN_NODE(),
+			GEO_NODE_START(),
+			GEO_OPEN_NODE(),
+				GEO_DISPLAY_LIST(LAYER_OPAQUE, new_koopa_root_cue_mesh_layer_1),
+			GEO_CLOSE_NODE(),
+			GEO_BRANCH(1, new_koopa_root_DHammer),
+			GEO_BRANCH(1, new_koopa_root_DPaddle),
+		GEO_CLOSE_NODE(),
 	GEO_CLOSE_NODE(),
 	GEO_RETURN(),
 };
@@ -103,14 +132,15 @@ const GeoLayout new_koopa_geo[] = {
 							GEO_OPEN_NODE(),
 								GEO_ANIMATED_PART(LAYER_OPAQUE, 54, 0, 0, new_koopa_000_offset_008_mesh_layer_1),
 								GEO_OPEN_NODE(),
-									GEO_SWITCH_CASE(3, geo_switch_level),
+									GEO_SWITCH_CASE(6, geo_switch_level),
 									GEO_OPEN_NODE(),
 										GEO_NODE_START(),
-										GEO_BRANCH(1, new_koopa_ForkArma),
-										GEO_BRANCH(1, new_koopa_PhoneArma),
-										GEO_BRANCH(1, new_koopa_ForkArma),
-										GEO_BRANCH(1, new_koopa_ForkArma),
-										GEO_BRANCH(1, new_koopa_IcicleArma),
+										GEO_BRANCH(1, new_koopa_AForkArma),
+										GEO_BRANCH(1, new_koopa_BPhoneArma),
+										GEO_BRANCH(1, new_koopa_AForkArma),
+										GEO_BRANCH(1, new_koopa_AForkArma),
+										GEO_BRANCH(1, new_koopa_CIcicleArma),
+										GEO_BRANCH(1, new_koopa_DCue),
 									GEO_CLOSE_NODE(),
 								GEO_CLOSE_NODE(),
 							GEO_CLOSE_NODE(),
