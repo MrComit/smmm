a47054838790d7df354068ffdd743c71dcce5469
MrComit <yozemight@gmail.com>
2022-03-24 20:42:34 -0400

    treehouse exterior

--- levels/hmc/area_2/geo.inc.c
+++ levels/hmc/area_2/geo.inc.c
@@ -130,6 +130,7 @@ const GeoLayout hmc_dl_211__PreHouse2_geo[] = {
 		GEO_ROTATION_NODE_WITH_DL(LAYER_OPAQUE, 0, 90, 0, hmc_dl_Z9_mesh_layer_1),
 		GEO_OPEN_NODE(),
 			GEO_DISPLAY_LIST(LAYER_ALPHA, hmc_dl_Z9_mesh_layer_4),
+			GEO_DISPLAY_LIST(LAYER_TRANSPARENT_DECAL, hmc_dl_Z9_mesh_layer_6),
 		GEO_CLOSE_NODE(),
 		GEO_DISPLAY_LIST(LAYER_ALPHA, hmc_dl_ZZTallGrass_001_mesh_layer_4),
 		GEO_DISPLAY_LIST(LAYER_OPAQUE, hmc_dl_ZZTallGrass_001_mesh_layer_1),
