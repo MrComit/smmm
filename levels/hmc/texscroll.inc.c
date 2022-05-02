054167f2b4df9ed8f74232338c0768a637f67c16
MrComit <yozemight@gmail.com>
2022-05-02 00:36:06 -0400

    sauna polishing and decorating

--- levels/hmc/texscroll.inc.c
+++ levels/hmc/texscroll.inc.c
@@ -434,7 +434,7 @@ void scroll_hmc_dl_MazeNoCol_mesh_layer_1_vtx_0() {
 	currentY += deltaY;	timeY += 1;
 }
 
-void scroll_hmc_dl_Sauna_mesh_layer_1_vtx_2() {
+void scroll_hmc_dl_Sauna_mesh_layer_1_vtx_7() {
 	int i = 0;
 	int count = 36;
 	int width = 32 * 0x20;
@@ -452,7 +452,7 @@ void scroll_hmc_dl_Sauna_mesh_layer_1_vtx_2() {
 	float amplitudeY = 0.699999988079071;
 	float frequencyY = 0.20000000298023224;
 	float offsetY = 32.0;
-	Vtx *vertices = segmented_to_virtual(hmc_dl_Sauna_mesh_layer_1_vtx_2);
+	Vtx *vertices = segmented_to_virtual(hmc_dl_Sauna_mesh_layer_1_vtx_7);
 
 	deltaX = (int)(amplitudeX * frequencyX * coss((frequencyX * timeX + offsetX) * (1024 * 16 - 1) / 6.28318530718) * 0x20);
 	deltaY = (int)(amplitudeY * frequencyY * coss((frequencyY * timeY + offsetY) * (1024 * 16 - 1) / 6.28318530718) * 0x20);
@@ -510,6 +510,39 @@ void scroll_sts_mat_hmc_dl_SaunaLavaDyn_layer1() {
 	shift_t(mat, 21, PACK_TILESIZE(0, 1));
 };
 
+void scroll_hmc_dl_Steam_mesh_layer_5_vtx_0() {
+	int i = 0;
+	int count = 32;
+	int width = 64 * 0x20;
+	int height = 64 * 0x20;
+
+	static int currentX = 0;
+	int deltaX;
+	static int timeX;
+	float amplitudeX = 0.30000001192092896;
+	float frequencyX = 0.10000000149011612;
+	float offsetX = 0.0;
+	static int currentY = 0;
+	int deltaY;
+	Vtx *vertices = segmented_to_virtual(hmc_dl_Steam_mesh_layer_5_vtx_0);
+
+	deltaX = (int)(amplitudeX * frequencyX * coss((frequencyX * timeX + offsetX) * (1024 * 16 - 1) / 6.28318530718) * 0x20);
+	deltaY = (int)(0.30000001192092896 * 0x20) % height;
+
+	if (absi(currentX) > width) {
+		deltaX -= (int)(absi(currentX) / width) * width * signum_positive(deltaX);
+	}
+	if (absi(currentY) > height) {
+		deltaY -= (int)(absi(currentY) / height) * height * signum_positive(deltaY);
+	}
+
+	for (i = 0; i < count; i++) {
+		vertices[i].n.tc[0] += deltaX;
+		vertices[i].n.tc[1] += deltaY;
+	}
+	currentX += deltaX;	timeX += 1;	currentY += deltaY;
+}
+
 void scroll_hmc() {
 	scroll_sts_mat_hmc_dl_Wall_layer1();
 	scroll_sts_mat_hmc_dl_WoodFloor_layer1();
@@ -537,8 +570,9 @@ void scroll_hmc() {
 	scroll_hmc_dl_Hallway_005_mesh_layer_1_vtx_5();
 	scroll_hmc_dl_Maze_mesh_layer_1_vtx_0();
 	scroll_hmc_dl_MazeNoCol_mesh_layer_1_vtx_0();
-	scroll_hmc_dl_Sauna_mesh_layer_1_vtx_2();
+	scroll_hmc_dl_Sauna_mesh_layer_1_vtx_7();
 	scroll_sts_mat_hmc_dl_SaunaLava_layer1();
 	scroll_hmc_dl_SaunaMat_mesh_layer_1_vtx_0();
 	scroll_sts_mat_hmc_dl_SaunaLavaDyn_layer1();
+	scroll_hmc_dl_Steam_mesh_layer_5_vtx_0();
 }
