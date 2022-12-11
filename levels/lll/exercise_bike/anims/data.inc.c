b98b39663fbd0e783129c8521bb2cc677c2caacd
MrComit <yozemight@gmail.com>
2022-12-11 17:24:21 -0500

    fixed exercise bike anim glitch

--- levels/lll/exercise_bike/anims/data.inc.c
+++ levels/lll/exercise_bike/anims/data.inc.c
@@ -1,1 +1,2 @@
 #include "anim_ExerciseBike_002Action.inc.c"
+#include "anim_anim2.inc.c"
--- levels/lll/exercise_bike/anims/table.inc.c
+++ levels/lll/exercise_bike/anims/table.inc.c
@@ -1,4 +1,5 @@
 const struct Animation *const exercise_bike_anims[] = {
 	&exercise_bike_anim_ExerciseBike_002Action,
+	&exercise_bike_anim_anim2,
 	NULL,
 };
