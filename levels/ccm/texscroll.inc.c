c1c50d92a24312f5923155cc760f7973f12c5677
MrComit <yozemight@gmail.com>
2023-02-18 20:58:20 -0500

    city texture updates

--- levels/ccm/texscroll.inc.c
+++ levels/ccm/texscroll.inc.c
@@ -1,8 +1,8 @@
-void scroll_sts_mat_ccm_dl_ScrapMetalDrip_001_layer6() {
-	Gfx *mat = segmented_to_virtual(mat_ccm_dl_ScrapMetalDrip_001_layer6);
+void scroll_sts_mat_ccm_dl_ScrapMetalDrip_002_layer6() {
+	Gfx *mat = segmented_to_virtual(mat_ccm_dl_ScrapMetalDrip_002_layer6);
 	shift_t(mat, 21, PACK_TILESIZE(0, 1));
 };
 
 void scroll_ccm() {
-	scroll_sts_mat_ccm_dl_ScrapMetalDrip_001_layer6();
+	scroll_sts_mat_ccm_dl_ScrapMetalDrip_002_layer6();
 }
--- levels/ccm/texscroll.inc.h
+++ levels/ccm/texscroll.inc.h
@@ -1,2 +1,2 @@
-extern void scroll_sts_mat_ccm_dl_ScrapMetalDrip_001_layer6();
+extern void scroll_sts_mat_ccm_dl_ScrapMetalDrip_002_layer6();
 extern void scroll_ccm();
