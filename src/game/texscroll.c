825a63f18f416ba8c2e6a69302164f4f50289f35
MrComit <yozemight@gmail.com>
2023-03-30 18:25:24 -0400

    art gallery structuring

--- src/game/texscroll.c
+++ src/game/texscroll.c
@@ -22,6 +22,7 @@
 #include "src/game/texscroll/ccm_texscroll.inc.c"
 #include "src/game/texscroll/hmc_texscroll.inc.c"
 #include "src/game/texscroll/lll_texscroll.inc.c"
+#include "src/game/texscroll/ssl_texscroll.inc.c"
 void scroll_textures() {
 	if(SCROLL_CONDITION(sSegmentROMTable[0x7] == (uintptr_t)_bob_segment_7SegmentRomStart)) {
 		scroll_textures_bob();
@@ -60,6 +61,10 @@ void scroll_textures() {
 
 	if(SCROLL_CONDITION(sSegmentROMTable[0x7] == (uintptr_t)_lll_segment_7SegmentRomStart)) {
 		scroll_textures_lll();
+	}
+
+	if(SCROLL_CONDITION(sSegmentROMTable[0x7] == (uintptr_t)_ssl_segment_7SegmentRomStart)) {
+		scroll_textures_ssl();
 	}
 
 }
--- src/game/texscroll.h
+++ src/game/texscroll.h
@@ -10,6 +10,7 @@
 #include "src/game/texscroll/ccm_texscroll.inc.h"
 #include "src/game/texscroll/hmc_texscroll.inc.h"
 #include "src/game/texscroll/lll_texscroll.inc.h"
+#include "src/game/texscroll/ssl_texscroll.inc.h"
 extern void scroll_textures();
 
 #endif
