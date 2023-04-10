4a41cb1dfb2141bbbca16bf2c779a361cf95c36b
MrComit <yozemight@gmail.com>
2023-04-10 00:22:28 -0400

    spinning plat uv effect

--- levels/ssl/spinning_plat/geo_header.h
+++ levels/ssl/spinning_plat/geo_header.h
@@ -1,7 +1,7 @@
 extern const GeoLayout spinning_plat_geo[];
 extern u8 spinning_plat_deepplanks_redbrown_rgba16[];
 extern u8 spinning_plat_i4_matt_multitex_i4[];
-extern Vtx spinning_plat_splat_mesh_layer_1_vtx_0[46];
+extern Vtx spinning_plat_splat_mesh_layer_1_vtx_0[61];
 extern Gfx spinning_plat_splat_mesh_layer_1_tri_0[];
 extern Gfx mat_spinning_plat_WoodFloorTrophyRoom_002_v5_layer1[];
 extern Gfx mat_revert_spinning_plat_WoodFloorTrophyRoom_002_v5_layer1[];
