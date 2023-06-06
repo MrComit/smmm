8295c17edf7d4eb7c41aadc40f70ad806cd213cd
MrComit <yozemight@gmail.com>
2023-06-06 00:08:33 -0400

    boss arena improvements

--- levels/ddd/texscroll.inc.h
+++ levels/ddd/texscroll.inc.h
@@ -1,4 +1,5 @@
 extern void scroll_sts_mat_ddd_dl_BooGooObjWall_layer1();
 extern void scroll_sts_mat_ddd_dl_BooGooDropperPillars_layer1();
 extern void scroll_sts_mat_ddd_dl_BooGooObjWall_001_layer1();
+extern void scroll_ddd_dl_MountainExterior_002_mesh_layer_4_vtx_0();
 extern void scroll_ddd();
