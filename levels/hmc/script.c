79c3bcdadd43f0877e2b6b9e9f60919819b49ded
MrComit <yozemight@gmail.com>
2023-08-04 23:35:12 -0400

    added some platforming to hallways

--- levels/hmc/script.c
+++ levels/hmc/script.c
@@ -18,6 +18,8 @@
 #include "actors/group0.h"
 #include "actors/group0.h"
 #include "actors/group0.h"
+#include "actors/group0.h"
+#include "actors/group0.h"
 
 /* Fast64 begin persistent block [includes] */
 #include "actors/group0.h"
