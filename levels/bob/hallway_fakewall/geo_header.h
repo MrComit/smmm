48cdbb929c3433a6c51d84c63269e7cb71ee2328
MrComit <yozemight@gmail.com>
2023-08-05 23:50:41 -0400

    l1 and l2 hallway textures revamped

--- levels/bob/hallway_fakewall/geo_header.h
+++ levels/bob/hallway_fakewall/geo_header.h
@@ -1,5 +1,5 @@
 extern const GeoLayout hallway_fakewall_geo[];
-extern u8 hallway_fakewall_ci4_hallway_wall3_i4[];
+extern u8 hallway_fakewall__32x64_newnew_hallway_wall_brighter_i8[];
 extern u8 hallway_fakewall__32x_multigrass_rgba16_lowcontrast_brightgreen_rgba16[];
 extern Vtx hallway_fakewall_hallDL_mesh_layer_4_vtx_0[4];
 extern Gfx hallway_fakewall_hallDL_mesh_layer_4_tri_0[];
